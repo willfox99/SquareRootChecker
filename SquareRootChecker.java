3e14934f90cd0ebbd8fd568b68a1f6fe8b35eeda
Kent Collins <kent.r.collins@gmail.com>
2014-10-12 15:57:22 +0100

    Create SquareRootChecker.java

---
+++ SquareRootChecker.java
@@ -0,0 +1,12 @@
+/**
+*
+* Prompts user to enter a value, checks if negative -- if not, prints out the square root
+*
+* @author <your name here>
+* @version <date you finished the code>
+*
+*/
+
+public class SquareRootChecker {
+
+}
